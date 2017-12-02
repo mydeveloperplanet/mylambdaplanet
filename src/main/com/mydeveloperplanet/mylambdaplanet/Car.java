432ef65e6c58e8f72ea2f8e25437a34f0f25b1e3
Gunter Rotsaert <gunter.rotsaert@triopsys.nl>
2017-12-02 14:41:10 +0100

    Added business object representing a Car

---
+++ src/main/com/mydeveloperplanet/mylambdaplanet/Car.java
@@ -0,0 +1,37 @@
+package com.mydeveloperplanet.mylambdaplanet;
+
+/**
+ * Business object representing a car
+ */
+public class Car {
+
+    private String brand;
+
+    private String type;
+
+    private int buildYear;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getBuildYear() {
+        return buildYear;
+    }
+
+    public void setBuildYear(int buildYear) {
+        this.buildYear = buildYear;
+    }
+}
